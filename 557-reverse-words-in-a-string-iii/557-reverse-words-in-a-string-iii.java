cb719ff443f9554c52b258eaffa0392e15dc850f
AakritiTripathi1092 <112464602+AakritiTripathi1092@users.noreply.github.com>
2022-09-22 23:21:24 +0530

    Time: 10 ms (63.52%), Space: 50.5 MB (53.76%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String reverseWords(String s) {
+        String []str=s.split(" ");
+        for(int i=0;i<str.length;i++){
+            str[i]=new StringBuilder(str[i]).reverse().toString();
+        }
+        return String.join(" ",str);
+    }
+}
+        
+        
+        
+       
